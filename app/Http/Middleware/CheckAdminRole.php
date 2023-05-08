cc185c5034543c25fabc916c6ac9ed7f5a91bb63
rezekmatija <matija.rezek@social3.hr>
2023-05-08 20:36:14 +0200

    dodan checkadminrole

---
+++ app/Http/Middleware/CheckAdminRole.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class CheckAdminRole
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle($request, Closure $next)
+{
+    if ($request->user() && $request->user()->role !== 'admin') {
+        return redirect('/dashboard'); // or abort(403, 'Unauthorized Access');
+    }
+
+    return $next($request);
+}
+}
